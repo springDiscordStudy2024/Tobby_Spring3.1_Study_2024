443f1a0db69fa073bcbf70f1890d461c679cd53c
nakyeonko3 <badaclock@gmail.com>
2024-03-23 05:50:15 +0900

    Docs : "README.md 파일 추가"
    README.md 파일에는 종속성과 설치 방법 등이 들어 있음.

--- nakyeonko3/src/main/java/com/group/todomvc/controller/TodoController.java
+++ nakyeonko3/src/main/java/com/group/todomvc/controller/TodoController.java
@@ -20,21 +20,26 @@ public class TodoController {
     public String greeting() {
         return "Hello, World";
     }
+
+    // todo를 추가하는 기능  create
     @PostMapping
     public void saveTodo(@RequestBody ToDoSaveRequest request) {
         todoService.saveTodo(request);
     }
 
+    //저장된 todo전체를 가져오는 기능 read
     @GetMapping("/list")
     public List<ToDoGetAllResponse> getAll(){
         return todoService.getAll();
     }
 
+    // 저장된 todo의 내용을 업데이트 하는 기능 update
     @PutMapping()
     public void updateTodo(@RequestParam long id){
         todoService.updateTodo(id);
     }
 
+    // 저장된 todo중에서 id 값을 이용해서 삭제하는 기능 delete
     @DeleteMapping()
     public void deleteTodo(@RequestParam long id){
         todoService.deleteTodo(id);
